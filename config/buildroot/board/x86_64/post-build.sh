54015ae1550aba52a8c048ebb960c737ea3e64cd
Lance R. Vick <lance@lrvick.net>
2023-02-15 04:52:28 -0800

    don't overwrite VERSION in /etc/environment

--- config/buildroot/board/x86_64/post-build.sh
+++ config/buildroot/board/x86_64/post-build.sh
@@ -9,7 +9,7 @@ BOARD_DIR="$(dirname $0)"
 cp -f ${BOARD_DIR}/grub.cfg ${TARGET_DIR}/boot/grub/grub.cfg
 
 echo "export VERSION=\"${VERSION}\"" > ${TARGET_DIR}/etc/environment
-echo "export GIT_REF=\"${GIT_REF}\"" > ${TARGET_DIR}/etc/environment
+echo "export GIT_REF=\"${GIT_REF}\"" >> ${TARGET_DIR}/etc/environment
 echo "export GIT_AUTHOR=\"${GIT_AUTHOR}\"" >> ${TARGET_DIR}/etc/environment
 echo "export GIT_KEY=\"${GIT_KEY}\"" >> ${TARGET_DIR}/etc/environment
 echo "export GIT_TIMESTAMP=\"${GIT_TIMESTAMP}\"" >> ${TARGET_DIR}/etc/environment
